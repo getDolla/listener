0e996ac7fc0e743f615f3a7aae4eef28d607b2d8
getDolla <ywang8@stuy.edu>
2017-03-09 14:51:10 -0500

    update

--- animation.js
+++ animation.js
@@ -15,37 +15,75 @@ var stopIt = function() {
 
 /* clear */
 var clear = function() {
-  while (svg.lastChild) {
-    svg.removeChild(svg.lastChild);
-  }
+    while (svg.lastChild) {
+	svg.removeChild(svg.lastChild);
+    }
 };
 
 /* closure structure */
-var animateCircle = function(evt) {
-  var rad = 1;
-  stopIt();
+var drawCircle = function(evt) {
+    var rad = 4;
 
-  var drawCircle = function(evt) {
-    clear();
+    var circle = function(evt) {
+	
+	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	c.setAttribute("cx", w/2);
+	c.setAttribute("cy", h/2);
+	c.setAttribute("stroke", "black");
+	c.setAttribute("stroke-width", "1");
+	c.setAttribute("fill", "#ffb732");
+	c.setAttribute("r", rad.toString());
+	svg.appendChild(c);
 
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", w/2);
-    c.setAttribute("cy", h/2);
-    c.setAttribute("stroke", "black");
-    c.setAttribute("stroke-width", "1");
-    c.setAttribute("fill", "#ffb732");
-    c.setAttribute("r", rad.toString());
-    svg.appendChild(c);
+    };
 
-    rid = window.requestAnimationFrame( drawCircle );
-  };
-
-  drawCircle();
+    circle();
 };
 
-circle.addEventListener('click', animateCircle );
+
+var animateCircle = function(evt) {
+
+    /* change the xval and yval = change in speed */
+    var xval = 2;
+    var yval = 2;
+
+    var animate = function(evt) {
+	var children = svg.children;
+	for(var i = 0; i < children.length; i++){
+	    var xcor = children[i].getAttribute("x");
+	    var ycor = children[i].getAttribute("y");
+
+	    children[i].setAttribute("href", src );
+	    children[i].setAttribute("x", xcor.toString());
+	    children[i].setAttribute("y", ycor.toString());
+	    children[i].setAttribute("height", "80");
+	    children[i].setAttribute("width", "120");
+
+	    /* if image hits borders */
+	    /* Note: different values are used instead of 0 and w/height due to image whitespace */
+	    xval = ( ((xcor + xval) <= -18) || ((xcor + xval) >= (w - 102) ) ) ? -xval : xval;
+	    yval = ( ((ycor + yval) <= -10) || ((ycor + yval) >= (h - 70) ) ) ? -yval : yval;
+
+	    xcor += xval;
+	    ycor += yval; 
+	}
+
+		
+	/* remember that images are drawn from the upper left hand corner */
+	var d = document.createElementNS("http://www.w3.org/2000/svg", "image");
+	
+
+	rid = window.requestAnimationFrame( animate );
+    };
+
+    animate();
+};
+
+
+svg.addEventListener('click', to_do() );
 
 /* stop animation */
-stop.addEventListener("click", function(evt) {
+clear.addEventListener("click", function(evt) {
     stopIt();
+    clear()
 });
