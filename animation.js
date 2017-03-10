fc55a6ce42c046f6bfaab3b7f318277af210a043
getDolla <ywang8@stuy.edu>
2017-03-09 21:07:53 -0500

    need to do to_do()

--- animation.js
+++ animation.js
@@ -8,6 +8,17 @@ var rid = 0;
 var w = svg.getAttribute("width")
 var h = svg.getAttribute("height")
 
+/* circle radius */
+var rad = 4;
+
+var mousex, mousey;
+
+/* keeps track of the xcor and ycor of mouse */
+svg.addEventListener("mousemove", function(evt) {
+    mousex = evt.offsetX.toString();
+    mousey = evt.offsetY.toString();
+});
+
 /* stop current animation */
 var stopIt = function() {
     window.cancelAnimationFrame( rid );
@@ -22,22 +33,21 @@ var clear = function() {
 
 /* closure structure */
 var drawCircle = function(evt) {
-    var rad = 4;
 
-    var circle = function(evt) {
-	
-	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	c.setAttribute("cx", w/2);
-	c.setAttribute("cy", h/2);
-	c.setAttribute("stroke", "black");
-	c.setAttribute("stroke-width", "1");
-	c.setAttribute("fill", "#ffb732");
-	c.setAttribute("r", rad.toString());
-	svg.appendChild(c);
+  var circle = function(evt) {
 
-    };
+  	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  	c.setAttribute("cx", mousex);
+  	c.setAttribute("cy", mousey);
+  	c.setAttribute("stroke", "black");
+  	c.setAttribute("stroke-width", "1");
+  	c.setAttribute("fill", "#551a8b");
+  	c.setAttribute("r", rad.toString());
+  	svg.appendChild(c);
 
-    circle();
+  };
+
+  circle();
 };
 
 
@@ -48,41 +58,46 @@ var animateCircle = function(evt) {
     var yval = 2;
 
     var animate = function(evt) {
-	var children = svg.children;
-	for(var i = 0; i < children.length; i++){
-	    var xcor = children[i].getAttribute("x");
-	    var ycor = children[i].getAttribute("y");
+	       var children = svg.children;
 
-	    children[i].setAttribute("href", src );
-	    children[i].setAttribute("x", xcor.toString());
-	    children[i].setAttribute("y", ycor.toString());
-	    children[i].setAttribute("height", "80");
-	    children[i].setAttribute("width", "120");
+         for(var i = 0; i < children.length; i++) {
 
-	    /* if image hits borders */
-	    /* Note: different values are used instead of 0 and w/height due to image whitespace */
-	    xval = ( ((xcor + xval) <= -18) || ((xcor + xval) >= (w - 102) ) ) ? -xval : xval;
-	    yval = ( ((ycor + yval) <= -10) || ((ycor + yval) >= (h - 70) ) ) ? -yval : yval;
+      	    var xcor = parseInt(children[i].getAttribute("x"));
+      	    var ycor = parseInt(children[i].getAttribute("y"));
 
-	    xcor += xval;
-	    ycor += yval; 
-	}
+      	    children[i].setAttribute("href", src );
+      	    children[i].setAttribute("x", xcor.toString());
+      	    children[i].setAttribute("y", ycor.toString());
 
-		
-	/* remember that images are drawn from the upper left hand corner */
-	var d = document.createElementNS("http://www.w3.org/2000/svg", "image");
-	
+      	    /* if image hits borders */
+      	    /* Note: different values are used instead of 0 and w/height due to image whitespace */
+      	    xval = ( ((xcor + xval) <= rad) || ((xcor + xval) >= (w - rad) ) ) ? -xval : xval;
+      	    yval = ( ((ycor + yval) <= rad) || ((ycor + yval) >= (h - rad) ) ) ? -yval : yval;
 
-	rid = window.requestAnimationFrame( animate );
+      	    xcor += xval;
+      	    ycor += yval;
+          }
+
+	        rid = window.requestAnimationFrame( animate );
     };
 
     animate();
 };
 
+var changecolor = function(evt) {
+  this.setAttribute("fill", "green");
+  evt.stopPropagation();
+};
+
+var removeCircle = function(evt) {
+  svg.removeChild(this);
+  evt.stopPropagation();
+};
 
 svg.addEventListener('click', to_do() );
+move.addEventListener('click', animateCircle() );
 
-/* stop animation */
+/* clear button */
 clear.addEventListener("click", function(evt) {
     stopIt();
     clear()
